ccdf87fdec901e383b3fc218bd3319b37a9e53b1
JiminHong-r <80619314+JiminHong-r@users.noreply.github.com>
2023-06-02 17:16:14 +0900

    [Silver III] Title: N과 M (7), Time: 1528 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+visited = []
+
+def dfs(visited):
+    if (len(visited) == m):
+        print(' '.join(map(str, visited)))
+        return
+    for i in num:
+        visited.append(i)
+        dfs(visited)
+        visited.pop()
+
+dfs(visited)
