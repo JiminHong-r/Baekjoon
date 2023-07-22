6cac02e47fad89b5fcc487ffbf4a47dab6bc4cb7
JiminHong-r <80619314+JiminHong-r@users.noreply.github.com>
2023-07-22 18:52:52 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+
+board = [input() for _ in range(n)]
+cnt = []
+
+for i in range(n - 7):
+	for j in range(m - 7):
+		idx1 = 0
+		idx2 = 0
+		for a in range(i, i + 8):
+			for b in range(j, j + 8):
+				if ((a + b) % 2 == 0):
+					if (board[a][b] != 'W'):
+						idx1 += 1
+					if (board[a][b] != 'B'):
+						idx2 += 1
+				else:
+					if (board[a][b] != 'B'):
+						idx1 += 1
+					if (board[a][b] != 'W'):
+						idx2 += 1
+		cnt.append(min(idx1, idx2))
+
+print(min(cnt))
